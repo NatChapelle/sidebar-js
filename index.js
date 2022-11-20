2eadc98180d76ce2ff85ddcbc373dc9630bc3017
NatChapelle <nathchapelle002@gmail.com>
2022-11-20 12:16:21 +0100

    sidebar toggle vs boolean method

---
+++ index.js
@@ -0,0 +1,27 @@
+const btn = document.getElementById("btn");
+const sideBar = document.getElementById("side-bar");
+let isVisible = false; //<--for boolean version only
+// console.log(isVisible);
+
+// btn.addEventListener("click", () => {
+    // sideBar.classList.toggle("is-visible");
+// });
+
+//boolean method
+btn.addEventListener("click", () => {
+isVisible =!isVisible;
+// console.log(isVisible);
+isVisible ? sideBar.classList.add("is-visible"): sideBar.classList.remove("is-visible");
+});
+
+
+
+// Créer un événement au click sur le hamburger menu pour sortir la sidebar, ranger la sidebar si on reclick dessus
+
+// Ranger la sidebar si on click sur le contenu principal
+
+// BONUS
+// Créer en css l'hamburger menu qui se transforme en croix
+
+// Aide
+// Priorités en CSS : id > classe > baliseHtml
